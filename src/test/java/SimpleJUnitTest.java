87af70137ef3785e6fdf4a2c7d550a8e5e3971d9
ShirleyFennette <dkopylov@qatools.ru>
2025-03-30 22:01:59 +0300

    added code + jpeg

--- src/test/java/SimpleJUnitTest.java
+++ src/test/java/SimpleJUnitTest.java
@@ -1,15 +1,53 @@
+import static com.codeborne.selenide.Condition;
+import static com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static jdk.internal.misc.ThreadFlock.open;
+import static com.codeborne.selenide.Selenide.*;
 
-public class SearchTests {
+public class Task3Test {
+    @BeforeEach
+    void setUp() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+
     @Test
-    void successfulSearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+    void Task3() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('#footer').remove()");
+        $("[id=firstName]").setValue("Danil");
+        $("[id=lastName]").setValue("Koppylov");
+        $("[id=userEmail]").setValue("danil.kopylov2015@yandex.ru").pressEnter();
+        $("label[class=custom-control-label]").click();
+        $("[id=userNumber]").setValue("79817571195");
+        $("[id=dateOfBirthInput]").click();
+        $("[class=react-datepicker__year-select]>[value='2992']").click();
+        $("[class=react-datepicker__month-select]>[value='1']").click();
+        $(".react-datepicker__month .react-datepicker__day--003").click();
+        $("[id=subjectsContainer]").click();
+        $("[id=subjectsInput]").setValue("Maths").pressEnter();
+        $("#hobbies-checkbox-1+label").click();
+        $("#hobbies-checkbox-2+label").click();
+        $("#hobbies-checkbox-3+label").click();
+        $("[id=uploadPicture]").uploadFromClasspath("photo_2025-02-26 14.16.43.jpeg");
+        $("[id=currentAddress]").click();
+        $("[id=currentAddress]").setValue("Mirgorodskaya 14");
+        $("[id=react-select-3-input]").setValue("NCR").pressEnter();
+        $("[id=react-select-4-input]").setValue("Delhi").pressEnter();
+        $("[id=submit]").submit();
+        $("[class=table-responsive]").shouldHave(Condition.text("Danil Kopylov"));
+        $("[class=table-responsive]").shouldHave(Condition.text("danil.kopylov2015@yandex.ru"));
+        $("[class=table-responsive]").shouldHave(Condition.text("3 January 1992"));
+        $("[class=table-responsive]").shouldHave(Condition.text("Male"));
+        $("[class=table-responsive]").shouldHave(Condition.text("79817571195"));
+        $("[class=table-responsive]").shouldHave(Condition.text("Maths"));
+        $("[class=table-responsive]").shouldHave(Condition.text("photo_2025-02-26 14.16.43"));
+        $("[class=table-responsive]").shouldHave(Condition.text("Mirgorodskaya 14"));
+        $("[class=table-responsive]").shouldHave(Condition.text("NCR Delhi"));
+        $("#closeLargeModal").click();
     }
 }
